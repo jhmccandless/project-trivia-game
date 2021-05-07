a3b69854a6cac2208aa11eb71a26cb84dc06130c
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-06 20:27:52 -0500

    trying the fetch to get questions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let APIObject = [];
+
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
+)
+  .then((response) => response.json())
+  //   .then((data) => data)
+  .then((results) => console.log(results));
+
+// console.log(APIObject);
