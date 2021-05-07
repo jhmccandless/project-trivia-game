fe5a9ffcc610e83d029a228ba9d44930aa46e0d8
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-07 00:26:18 -0500

    got the answer to display

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 "use strict";
 
 let APIObjectArr;
+let randomNum = 4;
 
 fetch(
   "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
 )
   .then((response) => response.json())
-  //   .then((data) => data)
-  .then((data) => (APIObjectArr = data.results));
-
-console.log(APIObjectArr);
+  // .then((data) => console.log(data))
+  .then((data) => {
+    document.getElementById("question-text").textContent =
+      data.results[randomNum].question;
+    document.getElementById("answers-text").textContent =
+      data.results[randomNum].correct_answer;
+  });
