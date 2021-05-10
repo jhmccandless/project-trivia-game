b78fbcc9be795a060b52c6a8ca0a477429d57316
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-10 01:29:58 -0500

    working on fetching async. now trying to retrieve values from my promise inside my class

--- script.js
+++ script.js
@@ -3,12 +3,54 @@
 class Game {
   // attributes: players, player turn
   // methods: init function(start/reset game), state whose turn it is
+  constructor(players, playerTurn) {
+    this.players = players;
+    this.playerTurn = playerTurn;
+  }
+
+  init = () => {};
 }
 
 class Player {
   // attributes: name, score, questions asked, answer player gave
   // Methods: score manipulation
+  constructor(name, score, questionAsked, answerGiven) {
+    this.name = name;
+    this.score = score;
+    this.questionAsked = questionAsked;
+    this.answerGiven = answerGiven;
+  }
+  questionsAskedArr = [];
+  ansGivenArr = [];
+
+  inceaseScore = (score) => {
+    score = this.score;
+    score++;
+  };
+  storeQuestionAsked = (quest) => (quest = [this.questionAsked, ...quest]);
+  storeAnswerGiven = (ans) => (ans = [this.answerGiven, ...ans]);
 }
+
+const url =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
+// const apiData = async (url) => {
+//   let questionsArray = [];
+//   const resolve = await fetch(url);
+//   const data = await resolve.json();
+//   for (let i = 0; i < data.results.length; i++) {
+//     let question = data.results[i];
+//     const classQuestion = new Question(
+//       question.question,
+//       question.correct_answer,
+//       question.incorrect_answers
+//     );
+//     // console.log(classQuestion);
+//     questionsArray = [classQuestion, ...questionsArray];
+//   }
+//   return questionsArray;
+// };
+// apiData(url);
 
 class QuestionSet {
   constructor(url) {
@@ -22,26 +64,24 @@ class QuestionSet {
   }
 
   fetchQuestionSet() {
+    let arr = [];
     fetch(this.url)
       .then((response) => response.json())
-      // .then((data) => console.log(data))
       .then((data) => {
         for (let i = 0; i < data.results.length; i++) {
           let question = data.results[i];
-          // console.log(question);
           const classQuestion = new Question(
             question.question,
             question.correct_answer,
             question.incorrect_answers
           );
-          // console.log("classQuestion", classQuestion);
-          // this.questions.push(classQuestion);
-          this.questions = [classQuestion, ...this.questions];
+          arr.push(classQuestion);
+          // this.questions.push(classQuestion.question);
+          // console.log(arr);
         }
-        // console.log(this.questions);
       });
+    return arr;
   }
-  // method: fetching data, for-loop data into question calss
 }
 
 class Question {
@@ -51,21 +91,27 @@ class Question {
     this.question = question;
     this.correctAnswer = correctAnswer;
     this.answerChoices = this.jumbleAnswers(correctAnswer, incorrectAnswers);
-
-    // process the answer choices.
-    // Means making a single array of answer choices by combing the correctAnswer with incorrectAnswer
-    // Now! We return a question class that has: question, correctAnswer, answerChoices
   }
 
   jumbleAnswers(correctAnswer, incorrectAnswer) {
-    // jumble baby
     return [correctAnswer, ...incorrectAnswer];
   }
 }
+console.log("first");
+const firstFetch = new QuestionSet(url).fetchQuestionSet();
+console.log("oof", firstFetch);
+console.log("last");
 
-const firstFetch = new QuestionSet(
-  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
-).fetchQuestionSet();
+// console.log(new QuestionSet(url));
+// console.log("oooof", `1. ${firstFetch}`);
+
+// apiData(url).then((arr) => console.log("oooof", arr));
+// .finally(console.log("oof"));
+
+// function callbackTry(cb) {
+//   console.log("inside oooof", cb);
+// }
+// callbackTry(firstFetch.question);
 
 // const firstFetch;
 
@@ -77,5 +123,3 @@ const firstFetch = new QuestionSet(
 // Now we need to make a game
 // make some players
 // Then we start the game (THIS CAN'T BE DONE UNTIL WE HAVE FINISHED LOADING THE QUESTIONS!) you will need to figure out how to make the code wait for this
-
-console.log("oooof", firstFetch.question);
