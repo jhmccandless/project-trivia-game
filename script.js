f17a6547ba1427ab907f773de87796f16a0735c3
Jason McCandless <jhmccandless1105@gmail.com>
2021-05-12 23:36:33 -0500

    got farther, able to put answers and questons onto page, next step make the website work

--- script.js
+++ script.js
@@ -1,4 +1,99 @@
 "use strict";
+
+// suggeted methods and attributes:
+// Game
+// game.start()
+// game.players
+// Player
+// player.name
+// player.score
+// player.wins
+// player.losses
+// player.questions
+// Question
+// question.answers
+// question.correct_answer
+// question.answer_is_correct(ans)
+
+/*
+try 3. new method after consideration
+- bring all questions and answers to the site at once.
+- have the anwers be clickable.
+bonus: hide the divs so only one question is seen at once (my origanal idea for this project), scramble which question show up, scramble answer choices;
+*/
+
+// url i am getting data from:
+const url =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
+// class to fetch questions
+class aquireData {
+  constructor(url) {
+    this.url = url;
+  }
+
+  // initiating fetch call
+  fetchQuestionSet() {
+    // this just unedited api data, needs json and further refining
+    return fetch(this.url);
+  }
+}
+
+// controls user interface and question/answer refinement
+class UIcontroller extends aquireData {
+  constructor(url) {
+    super(url);
+  }
+
+  // promises to help page not upload before data is ready
+  // seems like different than what i've seen but it works here
+  async questionSet() {
+    const resolve = await this.fetchQuestionSet();
+    const data = await resolve.json();
+    const results = await data.results;
+    // isolates data we need from the results
+    const narrowResults = this.refineQuestionSet(results);
+    // updates UI
+    this.updataUI(narrowResults);
+  }
+
+  // this is designed to take take what i need from the results
+  refineQuestionSet(results) {
+    let refinedSet = [];
+    for (let i = 0; i < results.length; i++) {
+      const refined = new Question(
+        results[i].question,
+        results[i].correct_answer,
+        results[i].incorrect_answers
+      );
+      refinedSet = [refined, ...refinedSet];
+    }
+    return refinedSet;
+  }
+
+  updataUI(results) {
+    const questionMap = results.map((question) => {
+      return `<h2>Question</h2><h3>${question.question}</h3><p>${question.answerChoices}</p>`;
+    });
+    $(".question-box").html(questionMap);
+  }
+}
+
+class Question {
+  // idea here is to use this to narrow down the api data into info that i need
+  constructor(question, correctAnswer, incorrectAnswers) {
+    this.question = question;
+    this.correctAnswer = correctAnswer;
+    this.answerChoices = this.combineAnswer(correctAnswer, incorrectAnswers);
+  }
+
+  combineAnswer(correct, incorrect) {
+    return [correct, ...incorrect];
+  }
+}
+
+new UIcontroller(url).questionSet();
+
 /*
 class Fetch {
   // get the data from an API
@@ -29,6 +124,9 @@ class UIcontroller extends fetch{
     $('#content').html(planetList)
   }
 */
+
+/*
+Try 2 that kinda worked. Able to get one question and ordered answers to log.
 
 // api database i am calling
 const url =
@@ -83,12 +181,16 @@ class Player {
   constructor(name, score) {
     this.name = name;
     this.score = score;
+    this.questionsAskedArray = questionsAskedArray;
+    this.answerGiven = answerGiven;
   }
   score = 0;
+  questionsAskedArray = [];
+  answerGiven = [];
 
   increaseScore(score) {
     score = this.score;
-    score++;
+    return score++;
   }
 }
 
@@ -97,29 +199,23 @@ class Game {
   constructor(questions) {
     this.questions = questions;
     this.questionNumber = rando;
+    this.score = 0;
   }
 
   // calling this in the promise, I can get specific data to put into DOM
   init(data) {
     data = this.questions[this.questionNumber];
     console.log(data.question);
-    console.log(data.answerChoices);
+    console.log(data.answerChoices.join(", "));
     // let ans = prompt("test");
     // console.log(Number(ans));
   }
 }
 
-/*
-class Game {
-  // attributes: players, player turn
-  // methods: init function(start/reset game), state whose turn it is
-  constructor(players, playerTurn) {
-    this.players = players;
-    this.playerTurn = playerTurn;
-  }
 
-  init = () => {};
-}
+// api database i am calling
+const url =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
 class Player {
   // attributes: name, score, questions asked, answer player gave
@@ -140,8 +236,6 @@ class Player {
   storeQuestionAsked = (quest) => (quest = [this.questionAsked, ...quest]);
   storeAnswerGiven = (ans) => (ans = [this.answerGiven, ...ans]);
 }
-
-
 
 // const apiData = async (url) => {
 //   let questionsArray = [];
@@ -164,16 +258,11 @@ class Player {
 class QuestionSet {
   constructor(url) {
     this.url = url;
-  }
-
-  questions = [];
-
-  get question() {
-    return this.questions;
+    this.questionsAnswersArr = [];
   }
 
   fetchQuestionSet() {
-    let arr = [];
+    // let arr = [];
     fetch(this.url)
       .then((response) => response.json())
       .then((data) => {
@@ -184,12 +273,12 @@ class QuestionSet {
             question.correct_answer,
             question.incorrect_answers
           );
-          arr.push(classQuestion);
+          this.questionsAnswersArr.push(classQuestion);
           // this.questions.push(classQuestion.question);
           // console.log(arr);
         }
       });
-    return arr;
+    return this.questionsAnswersArr;
   }
 }
 
@@ -206,6 +295,18 @@ class Question {
     return [correctAnswer, ...incorrectAnswer];
   }
 }
+
+class Game {
+  // attributes: players, player turn
+  // methods: init function(start/reset game), state whose turn it is
+  constructor(players, playerTurn) {
+    this.players = players;
+    this.playerTurn = playerTurn;
+  }
+
+  init = () => {};
+}
+
 console.log("first");
 const firstFetch = new QuestionSet(url).fetchQuestionSet();
 console.log("oof", firstFetch);
